8b6f778df2ecfc15d418b374f1d266df1b6f0f39
AyushSingh-022007 <suir8780@gmail.com>
2025-08-29 18:40:17 +0530

    Day 7

--- Q_14.c
+++ Q_14.c
@@ -17,7 +17,7 @@ Consonant
 int main()
 {
     char ch;
-    printf("Enter ");
+    printf("Enter character: ");
     scanf("%c",&ch);
     if(ch=='a' || ch=='i' || ch=='e'|| ch=='o'|| ch=='u')
     {
