5f1847fa4c6ae243b3a0671e66bc5b74f61cff39
AyushSingh-022007 <suir8780@gmail.com>
2025-09-26 09:45:21 +0530

    Day 34

--- Q_67.c
+++ Q_67.c
@@ -14,15 +14,15 @@ Output 1:
 int main()
 {
     int n, pos, val;
-    printf("Enter size of array: ");
+    printf("Enter size of array:");
     scanf("%d", &n);
     int arr[100]; 
-    printf("Enter array elements: ");
+    printf("Enter array elements:");
     for(int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    printf("Enter position and value to insert: ");
+    printf("Enter position and value to insert:");
     scanf("%d %d", &pos, &val);
     for(int i = n; i >= pos; i--)
     {
