38d73870a2a6850d105d77eb559302c4f1283809
AyushSingh-022007 <suir8780@gmail.com>
2025-11-06 22:25:19 +0530

    Day 72

---
+++ Q__122.c
@@ -0,0 +1,28 @@
+// Q122: Write a C program that opens an existing file (e.g., info.txt) and reads its contents using fgets(). The program should print all the lines to the console until EOF (end of file) is reached.
+
+/*
+Sample Test Cases:
+Input 1:
+File: info.txt (Content: Name: Rahul\nAge: 23)
+Output 1:
+Name: Rahul
+Age: 23
+
+*/
+#include <stdio.h>
+int main()
+{
+    FILE *fp;
+    char data[100];
+    if (fp == NULL)
+    {
+        printf("File not found!");
+        return 1;
+    }
+    while (fgets(data, sizeof(data), fp) != NULL)
+    {
+        printf("%s", data);
+    }
+    fclose(fp);
+    return 0;
+}
