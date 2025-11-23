f7f1733df69e67002f3ed651c405dba37dc01afb
AyushSingh-022007 <suir8780@gmail.com>
2025-11-23 15:28:49 +0530

    Day 75

--- Q__125.c
+++ Q__125.c
@@ -16,20 +16,27 @@ int main()
     FILE *file;
     char filename[50];
     char text[200];
-    printf("Enter the filename: ");
+    printf("enter the filename: ");
     fgets(filename, sizeof(filename), stdin);
-    filename[strcspn(filename, "\n")] = '\0';
+    for (int i = 0; filename[i] != '\0'; i++)
+    {
+        if (filename[i] == '\n')
+        {
+            filename[i] = '\0';
+            break;
+        }
+    }
     file = fopen(filename, "a");
     if (file == NULL)
     {
-        printf("Error! Could not open the file.\n");
+        printf("error");
         return 1;
     }
     printf("Enter text to append: ");
     fgets(text, sizeof(text), stdin);
     fputs(text, file);
     fclose(file);
-    printf("File updated successfully with appended text.\n");
+    printf("updated");
     return 0;
 }
 
