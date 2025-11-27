8bb47494ce675dffe90f138091a01147b821d515
AyushSingh-022007 <suir8780@gmail.com>
2025-11-27 22:11:57 +0530

    Day 99

--- Q__149.c
+++ Q__149.c
@@ -1,3 +1,13 @@
+//Q149: Use malloc() to allocate structure memory dynamically and print details.
+
+/*
+Sample Test Cases:
+Input 1:
+Student allocated dynamically with details: Tina 105 88
+Output 1:
+Name: Tina | Roll: 105 | Marks: 88
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 struct Student {
